40559c6c14b2d699d0e699aa63fd42e2213fa360
Shannon Ritenour <shannonritenour@gmail.com>
2021-02-16 13:12:29 -0600

    finished main project, still need to add above and beyond portion

--- app.js
+++ app.js
@@ -1,49 +1,63 @@
-let dieCounter = 1;
-
 // store each Die you create in a global array
 let dieArray = []; 
+let counter = 1;
 
 const container = document.createElement('main');
 container.className = 'container';
 document.body.appendChild(container);
+
 const generateDieBtn = document.getElementById('generate-die-btn');
+const rerollDieBtn = document.getElementById('reroll-die-btn');
+const sumDieBtn = document.getElementById('sum-die-btn');
 
 
 // Create a class named Die that represents a single die.
 class Die {
     constructor() {
         this.div = document.createElement('div');
-        this.value = document.createTextNode(dieCounter);
-        this.rendor();
+        this.value = document.createTextNode(counter);
+        this.rendor();                     
         this.roll();
-        
+        dieArray.push(this);        
     }
 
     // method named roll that generates a random integer 1-6, sets the value property, and updates the div on the page with the new value.
     roll() {
-        this.value = Math.floor((Math.random() * 6) + 1);        
-    }
-
-    // addEvents () {
-    //     this.div.addEventListener('mouseover', () => this.div.appendChild(this.value));
-    //     this.div.addEventListener('mouseout', () => this.div.removeChild(this.value));
-    //     this.div.addEventListener('click', () => this.div.style.backgroundColor = this.randomColor());
-    //     this.div.addEventListener('dblclick', () => this.destroySomething());
-    // }
-
-    
+        this.value = Math.floor((Math.random() * 6) + 1);   
+        this.div.textContent = this.value;      
+    }     
 
     rendor() {
         this.div.className = 'die';
-        this.div.id = dieCounter;
+        this.div.id = counter;       
+        this.div.appendChild(this.value); 
         container.appendChild(this.div);
-    }
-    
+    }    
 }
 
-generateDieBtn.addEventListener('click', function () {
-    new Die();    
-    dieCounter++;
+generateDieBtn.addEventListener('click', ()  => {
+    new Die();        
+    counter++;
 })
 
+rerollDieBtn.addEventListener('click', () => {    
+    for(let i = 0; i < dieArray.length; i++) {
+        dieArray[i].roll();
+    }
+})
 
+sumDieBtn.addEventListener('click', () => {
+    let sum = 0;
+    for(let i = 0; i < dieArray.length; i++) {
+        sum += dieArray[i].value;
+    }
+    alert(`The sum of the die is ${sum}.`);
+} )
+
+// this.div.addEventListener('click', () => {
+//     this.roll();
+// })
+
+// this.div.addEventListener('dblclick', () => {
+    
+// })
