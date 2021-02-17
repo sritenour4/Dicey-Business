bf3f12f16c45e60a58a0912e4f21446a68ba6e74
Shannon Ritenour <shannonritenour@gmail.com>
2021-02-17 09:45:51 -0600

    finished

--- app.js
+++ app.js
@@ -18,7 +18,23 @@ class Die {
         this.value = document.createTextNode(counter);
         this.rendor();                     
         this.roll();
-        dieArray.push(this);        
+
+        // The push() method adds one or more elements to the end of an array and returns the new length of the array.
+        dieArray.push(this);    
+              
+        // Add a feature where clicking on a die on the page causes just that one die to roll, updating its face value
+        this.div.addEventListener('click', () => this.roll());
+        
+        // Add a feature where double clicking on a die on the page causes that die to be removed from the page
+        // Be careful to take appropriate measures so that the sumDice function still reports the correct sum after you delete a die. 
+        this.div.addEventListener('dblclick', () => {
+            this.div.remove();
+            // The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+            let index = dieArray.indexOf(this);
+            if(index !== -1) {
+                dieArray.splice(index, 1);
+            }
+        })
     }
 
     // method named roll that generates a random integer 1-6, sets the value property, and updates the div on the page with the new value.
@@ -32,20 +48,24 @@ class Die {
         this.div.id = counter;       
         this.div.appendChild(this.value); 
         container.appendChild(this.div);
-    }    
+    } 
+        
 }
 
+// Clicking the button should create a new Die object (thus causing a div to be added to the screen with a random number 1-6)
 generateDieBtn.addEventListener('click', ()  => {
     new Die();        
     counter++;
 })
 
+// button which should call the roll method on all the dice, updating their values
 rerollDieBtn.addEventListener('click', () => {    
     for(let i = 0; i < dieArray.length; i++) {
         dieArray[i].roll();
     }
 })
 
+// Create a sumDice function and a button that triggers it. It should add up the current face value of all the dice on the page and display an alert with the sum.
 sumDieBtn.addEventListener('click', () => {
     let sum = 0;
     for(let i = 0; i < dieArray.length; i++) {
@@ -54,10 +74,3 @@ sumDieBtn.addEventListener('click', () => {
     alert(`The sum of the die is ${sum}.`);
 } )
 
-// this.div.addEventListener('click', () => {
-//     this.roll();
-// })
-
-// this.div.addEventListener('dblclick', () => {
-    
-// })
